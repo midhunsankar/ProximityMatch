7258429d913accde8ae9763be1014a3d126af2af
midhun <midhunsankar@live.com>
2018-05-05 16:48:27 +1200

    fullscan method added for testing

--- ProximityMatch/Vector.cs
+++ ProximityMatch/Vector.cs
@@ -11,17 +11,23 @@ namespace ProximityMatch
         private readonly int _dimension;
         protected readonly SortedList<double, LinkedList<VectorNode>> _index;
 
+        protected readonly IList<VectorNode> _indexfull;
+
 
         protected class VectorNode
         {
             public IVector _vector;
             public double[] _anglePlain;
+            public double _distanceOrgin;
+            public double _distance;
         }
 
         public Vector(int dimension = 3)
         {
             _dimension = dimension;
             _index = new SortedList<double, LinkedList<VectorNode>>();
+
+            _indexfull = new List<VectorNode>();
         }
 
         public void Plot(IVector vector)
@@ -33,7 +39,8 @@ namespace ProximityMatch
             var v = new VectorNode()
                 {
                     _vector = vector,
-                    _anglePlain = generateAngles(vector)
+                    _anglePlain = generateAngles(vector),
+                    _distanceOrgin = distance(new double[3]{0, 0, 0} , vector.coordinate)
                 };
 
             if(_index.ContainsKey(v._anglePlain[0]))
@@ -45,7 +52,111 @@ namespace ProximityMatch
                 var LinkedL = new LinkedList<VectorNode>();                
                 LinkedL.AddFirst(v);
                 _index.Add(v._anglePlain[0], LinkedL);
-            }         
+            }
+
+            _indexfull.Add(v);
+        }
+
+        public IList<IVector> nearest(IVector In, int take = 10)
+        {
+            var angle = generateAngles(In);
+            var lower = angle[0] - 3;
+            var upper = angle[0] + 3;
+            bool forward = true, backward = true;
+            List<VectorNode> candidates = new List<VectorNode>();
+            var index = _index.IndexOfKey(angle[0]);
+            int i = index;
+
+            foreach (var E in _index.ElementAt(i).Value.ToList())
+            {
+                E._distance = distance(In.coordinate, E._vector.coordinate);
+                if (InRange(angle, E._anglePlain, 3) && E._distance < 10)
+                {
+                    candidates.Add(E);
+                }
+            }
+
+            while (forward)
+            {
+                i++;
+                if (i < _index.Count)
+                {
+                    var L = _index.ElementAt(i).Key;
+                    if (L <= upper)
+                    {
+                        foreach (var E in _index.ElementAt(i).Value.ToList())
+                        {
+                            E._distance = distance(In.coordinate, E._vector.coordinate);
+                            if (InRange(angle, E._anglePlain, 3) && E._distance < 10)
+                            {
+                                candidates.Add(E);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        forward = false;
+                    }
+                }
+                else
+                {
+                    forward = false;
+                }
+            }
+            i = index;
+            while (backward)
+            {
+                i--;
+                if (i >= 0)
+                {
+                    var L = _index.ElementAt(i).Key;
+                    if (L >= lower)
+                    {
+                        foreach (var E in _index.ElementAt(i).Value.ToList())
+                        {
+                            E._distance = distance(In.coordinate, E._vector.coordinate);
+                            if (InRange(angle, E._anglePlain, 3) && E._distance < 10)
+                            {
+                                candidates.Add(E);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        backward = false;
+                    }
+                }
+                else
+                {
+                    backward = false;
+                }
+            }
+            return candidates.OrderBy(x =>x._distance).Take(take).Select<VectorNode, IVector>(x => x._vector).ToList();
+        }
+
+        public IList<IVector> nearestFull(IVector In, int take = 10)
+        {
+            List<VectorNode> candidates = new List<VectorNode>();
+
+            foreach (var E in _indexfull)
+            {
+                E._distance = distance(In.coordinate, E._vector.coordinate);
+                if (E._distance < 10)
+                {
+                    candidates.Add(E);
+                }
+            }
+            return candidates.OrderBy(x => x._distance).Take(take).Select<VectorNode, IVector>(x => x._vector).ToList();
+        }
+
+        public IList<IVector> getAll()
+        {
+            List<IVector> all = new List<IVector>();
+            foreach (var n in _index)
+            {
+                all.AddRange(n.Value.Select<VectorNode, IVector>(x => x._vector).ToList());
+            }
+            return all;
         }
 
         private double[] generateAngles(IVector vector)
@@ -89,80 +200,6 @@ namespace ProximityMatch
                 d += Math.Pow((co1[i] - co2[i]), 2);
             }
             return Math.Sqrt(d);
-        }
-
-        public IList<IVector> nearest(IVector In)
-        {
-            var angle = generateAngles(In);
-            var lower = angle[0] - 3;
-            var upper = angle[0] + 3;
-            bool forward = true, backward = true;
-            List<VectorNode> candidates = new List<VectorNode>();            
-            var index = _index.IndexOfKey(angle[0]);
-            int i = index;
-            
-            foreach (var E in _index.ElementAt(i).Value.ToList())
-            {
-                if (InRange(angle, E._anglePlain, 3) && distance(angle, E._anglePlain) < 1)
-                {
-                    candidates.Add(E);
-                }
-            }
-
-            while (forward)
-            {
-                i++;
-                if (i < _index.Count)
-                {
-                    var L = _index.ElementAt(i).Key;
-                    if (L <= upper)
-                    {
-                        foreach (var E in _index.ElementAt(i).Value.ToList())
-                        {
-                            if (InRange(angle, E._anglePlain, 3) && distance(angle, E._anglePlain) < 1)
-                            {
-                                candidates.Add(E);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        forward = false;
-                    }
-                }
-                else
-                {
-                    forward = false;
-                }
-            }
-            i = index;
-            while (backward)
-            {
-                i--;
-                if (i >= 0)
-                {
-                    var L = _index.ElementAt(i).Key;
-                    if (L >= lower)
-                    {
-                        foreach (var E in _index.ElementAt(i).Value.ToList())
-                        {
-                            if (InRange(angle, E._anglePlain, 3) && distance(angle, E._anglePlain) < 1)
-                            {
-                                candidates.Add(E);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        backward = false;
-                    }
-                }
-                else
-                {
-                    backward = false;
-                }
-            }
-            return candidates.Select<VectorNode, IVector>(x => x._vector).ToList();
         }
 
         private bool InRange(double[] co1, double[] co2, double deviation)
--- ProximityMatchApp/Program.cs
+++ ProximityMatchApp/Program.cs
@@ -19,52 +19,90 @@ namespace ProximityMatchApp
         static void Main(string[] args)
         {
             Vector carList = new Vector();
-
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
             //IList<Car> carList = new List<Car>();
             /* Adding some data to the list. */
-            carList.Plot(new Car() { refno = "car#1", xaxis = 0, yaxis = 2.018, zaxis = 30 });
+            //carList.Plot(new Car() { refno = "car#1", xaxis = 0, yaxis = 2018, zaxis = 30 });
 
-            carList.Plot(new Car() { refno = "car#2", xaxis = 0, yaxis = 2.018, zaxis = 25 });
-            carList.Plot(new Car() { refno = "car#3", xaxis = 0, yaxis = 2.018, zaxis = 50 });
-            carList.Plot(new Car() { refno = "car#4", xaxis = 1, yaxis = 2.018, zaxis = 40 });
-            carList.Plot(new Car() { refno = "car#5", xaxis = 10, yaxis = 2.015, zaxis = 30 });
-            carList.Plot(new Car() { refno = "car#6", xaxis = 30, yaxis = 2.016, zaxis = 25 });
-            carList.Plot(new Car() { refno = "car#7", xaxis = 0, yaxis = 2.017, zaxis = 50 });
-            carList.Plot(new Car() { refno = "car#8", xaxis = 70, yaxis = 2.010, zaxis = 10 });
-            carList.Plot(new Car() { refno = "car#9", xaxis = 0, yaxis = 2.018, zaxis = 22 });
-            carList.Plot(new Car() { refno = "car#10", xaxis = 5, yaxis = 2.017, zaxis = 45 });
-            carList.Plot(new Car() { refno = "car#11", xaxis = 0, yaxis = 2.018, zaxis = 35 });
-            carList.Plot(new Car() { refno = "car#12", xaxis = 2, yaxis = 2.017, zaxis = 40 });
-            carList.Plot(new Car() { refno = "car#13", xaxis = 10, yaxis = 2.015, zaxis = 21 });
-            carList.Plot(new Car() { refno = "car#14", xaxis = 8, yaxis = 2.016, zaxis = 25 });
-            carList.Plot(new Car() { refno = "car#15", xaxis = 3, yaxis = 2.017, zaxis = 35 });
-            carList.Plot(new Car() { refno = "car#16", xaxis = 60, yaxis = 2.011, zaxis = 11 });
-            carList.Plot(new Car() { refno = "car#17", xaxis = 60, yaxis = 2.014, zaxis = 11 });
-            carList.Plot(new Car() { refno = "car#18", xaxis = 35, yaxis = 2.016, zaxis = 22 });
-            carList.Plot(new Car() { refno = "car#19", xaxis = 0, yaxis = 2.017, zaxis = 45 });
-            carList.Plot(new Car() { refno = "car#20", xaxis = 20, yaxis = 2.013, zaxis = 20 });
+            //carList.Plot(new Car() { refno = "car#2", xaxis = 0, yaxis = 2018, zaxis = 25 });
+            //carList.Plot(new Car() { refno = "car#3", xaxis = 0, yaxis = 2018, zaxis = 50 });
+            //carList.Plot(new Car() { refno = "car#4", xaxis = 1, yaxis = 2018, zaxis = 40 });
+            //carList.Plot(new Car() { refno = "car#5", xaxis = 10, yaxis = 2015, zaxis = 30 });
+            //carList.Plot(new Car() { refno = "car#6", xaxis = 30, yaxis = 2016, zaxis = 25 });
+            //carList.Plot(new Car() { refno = "car#7", xaxis = 0, yaxis = 2017, zaxis = 50 });
+            //carList.Plot(new Car() { refno = "car#8", xaxis = 70, yaxis = 2010, zaxis = 10 });
+            //carList.Plot(new Car() { refno = "car#9", xaxis = 0, yaxis = 2018, zaxis = 22 });
+            //carList.Plot(new Car() { refno = "car#10", xaxis = 5, yaxis = 2017, zaxis = 45 });
+            //carList.Plot(new Car() { refno = "car#11", xaxis = 0, yaxis = 2018, zaxis = 35 });
+            //carList.Plot(new Car() { refno = "car#12", xaxis = 2, yaxis = 2017, zaxis = 40 });
+            //carList.Plot(new Car() { refno = "car#13", xaxis = 10, yaxis = 2015, zaxis = 21 });
+            //carList.Plot(new Car() { refno = "car#14", xaxis = 8, yaxis = 2016, zaxis = 25 });
+            //carList.Plot(new Car() { refno = "car#15", xaxis = 3, yaxis = 2017, zaxis = 35 });
+            //carList.Plot(new Car() { refno = "car#16", xaxis = 60, yaxis = 2011, zaxis = 11 });
+            //carList.Plot(new Car() { refno = "car#17", xaxis = 60, yaxis = 2014, zaxis = 11 });
+            //carList.Plot(new Car() { refno = "car#18", xaxis = 35, yaxis = 2016, zaxis = 22 });
+            //carList.Plot(new Car() { refno = "car#19", xaxis = 0, yaxis = 2017, zaxis = 45 });
+            //carList.Plot(new Car() { refno = "car#20", xaxis = 20, yaxis = 2013, zaxis = 20 });
 
-            var nodes = carList.nearest(new Car() { refno = "car#14", xaxis = 8, yaxis = 2.016, zaxis = 25 });
+            Random rnd = new Random();
+            double x, y, z;
+            int maxlimit = 1000000;
+            sw.Start();
 
-            foreach (Car dat in nodes)
+            for (int i = 0; i < maxlimit; i++)
             {
-                Console.WriteLine("refno = {0} , odometer = {1}K km , year = {2} , price = ${3}K , coordinate = ({1}, {2}, {3})",
-                                   dat.refno, dat.xaxis, dat.yaxis, dat.zaxis);
+                x = rnd.Next(0, 200);
+                y = rnd.Next(1965, 2018);
+                z = rnd.Next(1000, 100000);
+                z = z - z % 500;
+                carList.Plot(new Car() { refno = "car#" + i, xaxis = x, yaxis = y, zaxis = z });
             }
+            sw.Stop();
+            Console.WriteLine("\nPloting finished!! Time: {0} ms Cycles: {1} ticks", sw.Elapsed.TotalSeconds, sw.Elapsed.Ticks);
+            //foreach (Car dat in carList.getAll())
+            //{
+            //    Console.WriteLine("refno = {0} , odometer = {1}K km , year = {2} , price = ${3}K , coordinate = ({1}, {2}, {3})",
+            //                       dat.refno, dat.xaxis, dat.yaxis, dat.zaxis);
+            //}
+            do
+            {
+                x = rnd.Next(0, 200);
+                y = rnd.Next(1965, 2018);
+                z = rnd.Next(1000, 100000);
+                z = z - z % 500;
+                var car = new Car() { refno = "car#" + rnd.Next(0, 100000), xaxis = x, yaxis = y, zaxis = z };
 
-            //Graph gp = new Graph(In: carList);
+                Console.WriteLine("\nrefno = {0} , odometer = {1}K km , year = {2} , price = ${3} , coordinate = ({1}, {2}, {3})",
+                                       car.refno, car.xaxis, car.yaxis, car.zaxis);
+                Console.WriteLine("\n*******************************************************************************************\n");                
+                
+                sw = new System.Diagnostics.Stopwatch();
+                sw.Start();
+                var nodes = carList.nearest(car);
+                foreach (Car dat in nodes)
+                {
+                    Console.WriteLine("refno = {0} , odometer = {1}K km , year = {2} , price = ${3} , coordinate = ({1}, {2}, {3})",
+                                       dat.refno, dat.xaxis, dat.yaxis, dat.zaxis);
+                }
+                sw.Stop();
+                Console.WriteLine("\nSearching finished!! Time: {0} ms Cycles: {1} ticks", sw.Elapsed.TotalSeconds, sw.Elapsed.Ticks);
+
+                sw = new System.Diagnostics.Stopwatch();
+                sw.Start();
+                nodes = carList.nearestFull(car);
+                foreach (Car dat in nodes)
+                {
+                    Console.WriteLine("refno = {0} , odometer = {1}K km , year = {2} , price = ${3} , coordinate = ({1}, {2}, {3})",
+                                       dat.refno, dat.xaxis, dat.yaxis, dat.zaxis);
+                }
+                sw.Stop();
+                Console.WriteLine("\nSearching finished (Full)!! Time: {0} ms Cycles: {1} ticks", sw.Elapsed.TotalSeconds, sw.Elapsed.Ticks);
+
+                Console.WriteLine("\nDo you want to continue (y/n)?");
+            } while (Console.ReadKey(true).Key == ConsoleKey.Y);
             
-            //gp.DataList();
-
-            ////// Un comment the below line of code to view all proximities from a perticular point of reference to all other vector points.
-            //// gp.ListAllProximities("car#4");
-
-            //gp.ListProximities("car#4", 10);
-            //gp.ListProximities("car#8", 10);
-            //gp.ListProximities("car#13", 10);
-            
-            Console.Read();
         }
+        
     }
 
     /// <summary>
